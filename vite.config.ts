1cd759f1d95a6ccaeb5fccb4a4b4ea989dcbab04
Akhmad Sudaisi <akhmadsudaisi31@gmail.com>
2026-01-05 09:29:20 +0700

    revert: remove all PWA settings and configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: true
-      },
-      includeAssets: ['192.png', '512.png'],
-      manifest: {
-        name: 'RM SatSet',
-        short_name: 'RM',
-        description: 'Aplikasi SatSet Rekam Medis',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
 })
